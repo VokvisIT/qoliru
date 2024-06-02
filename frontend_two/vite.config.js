d05aaa809f3810de5907650e53f71825fe11d126
Ivan Sivkov <Vanu20011205@yandex.ru>
2024-06-02 19:37:01 +0300

    fix

--- frontend_two/vite.config.js
+++ frontend_two/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {    // <-- this object is added
+    port: 8080
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
