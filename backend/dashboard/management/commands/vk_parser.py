59ab1504a06de943f30c88fd250c6992b4cf09c5
Ivan Sivkov <Vanu20011205@yandex.ru>
2024-06-07 12:54:14 +0300

    Add models tracked by Git LFS2

--- backend/dashboard/management/commands/vk_parser.py
+++ backend/dashboard/management/commands/vk_parser.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         token = 'vk1.a.5uEA0WWcvx91FS4UhMFvjdRzjJCoV1JlijQphi0paAaocmJysNkFvhxGL40l7SSrUDHSBHo7waTME8bC38pYqrjURbgsW0Dn8MfVOJ98d753CZOfLgN_MpDFDEZ7GcLp1u_Ga9dQA2pv83N6h4pQY6q97Nt6JPx88ZmtzHTszQe_bNeX4Ecj4yYgTcYtd2GTKe9QPLj0bANiyWZFhsNI3w'
         version = '5.131'
-        end_date = datetime.now(pytz.timezone('Europe/Moscow')) - timedelta(days=30)
+        end_date = datetime.now(pytz.timezone('Europe/Moscow')) - timedelta(days=7)
         resources = Resource.objects.filter(source__name='ВКонтакте')
         for resource in resources:
             group_id = get_group_id(token, version, resource.link)
