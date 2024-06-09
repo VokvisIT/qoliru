aee7860f6c69263ce5951fc6f55996386637b37f
Ivan Sivkov <Vanu20011205@yandex.ru>
2024-06-09 20:18:19 +0300

    Test api 7

--- frontend_two/src/components/Stats.vue
+++ frontend_two/src/components/Stats.vue
@@ -104,7 +104,6 @@ export default {
   methods: {
     fetchBestRegionQOL() {
         this.loading = true
-        // DD
         axios.get('${process.env.VUE_APP_API_URL}/api/v1/dashboard/best-region-qol/')
         .then(response => {
         this.bestRegion = response.data
